1dc653c3a78a352ca2ecb208c93d69ec97ec3583
Halle <hallegj@googlemail.com>
2022-02-12 20:46:47 +0000

    remade alen

--- SpaceTD/Assets/Scripts/Enemy.cs
+++ SpaceTD/Assets/Scripts/Enemy.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public static void Create(Vector3 startPosition)
     {
-        
+        Instantiate(GameAssets.i.pfEnemy, startPosition, Quaternion.identity);
     }
 
     // Update is called once per frame
