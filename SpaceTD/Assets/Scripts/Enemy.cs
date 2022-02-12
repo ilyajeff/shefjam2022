e369997aa045a277c78b85e1b888715c9324698d
Halle <hallegj@googlemail.com>
2022-02-12 20:00:16 +0000

    Wave Spawning

--- SpaceTD/Assets/Scripts/Enemy.cs
+++ SpaceTD/Assets/Scripts/Enemy.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
+
 {
     // Start is called before the first frame update
-    void Start()
+    public static void Create(Vector3 startPoint)
     {
-        
+        Instantiate(GameAssets.i.pfEnemy, startPoint, Quaternion.identity);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
+
+
 }
---
+++ SpaceTD/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class WaveSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.B))
+        //Vector3 startVector = new Vector3(-2370.0F, 750.0F, 0.0F);
+        //Enemy.Create(startVector);
+    }
+
+    
+}
